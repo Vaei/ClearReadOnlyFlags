0b7d38b960cd612d2f7f8a35c5e09d7fe4aedb96
Vaei <jaredtaylor.99@gmail.com>
2023-06-09 16:14:09 +1200

    initial commit

---
+++ ClearReadOnlyFlags/Program.cs
@@ -0,0 +1,28 @@
+﻿string ProjectDir = Directory.GetCurrentDirectory();
+string PluginsDir = ProjectDir + "\\Plugins";
+
+if (Directory.Exists(ProjectDir))
+{
+    string[] ProjectBinaries = Directory.GetDirectories(ProjectDir, "Binaries", SearchOption.AllDirectories);
+    string[] PluginBinaries = Directory.GetDirectories(PluginsDir, "Binaries", SearchOption.AllDirectories);
+
+    List<string> Binaries = new List<string>();
+    foreach (string b in ProjectBinaries)
+    {
+        Binaries.Add(b);
+    }
+    foreach (string b in PluginBinaries)
+    {
+        Binaries.Add(b);
+    }
+
+    foreach (string d in Binaries)
+    {
+        string[] Files = Directory.GetFiles(d, "*", SearchOption.AllDirectories);
+        foreach (string f in Files)
+        {
+            FileInfo fi = new FileInfo(f);
+            fi.IsReadOnly = false;
+        }
+    }
+}
